0c8d7e5291f6ae2f6aa7205afb45472c539236bd
Sanjeewa Fernando <sanjeewafern@gmail.com>
2020-09-16 11:18:46 +0530

    fix(pencil): second release

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ const App = () => {
 
   return (
     <div className='app-container'>
+      {/* This is a sample comment */}
       <div className='temperature-display-container'>
         <div className={`temperature-display ${temperatureColor}`}>{initTemp}°C</div>
       </div>
